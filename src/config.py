8ce0c96d7be4644900307a58f7373f29bb6667a6
Sreejith <sreejimunthikodu@yahoo.com>
2020-07-18 17:01:47 -0700

    added connection details to config.py

--- src/config.py
+++ src/config.py
@@ -1,1 +1,21 @@
-# Define connection details
+import os
+
+DB_DETAILS = {
+    'dev': {
+        'mysql': {
+            'user': my_admin,
+            'password': mypassword,
+            'host': '3.96.15.164',
+            'port': 3306,
+            'database': covid_db_mysql
+        }
+        'psql': {
+            'user': my_admin,
+            'password': mypassword,
+            'host': '3.96.15.164',
+            'port': 5432,
+            'database': covid_db_psql
+        }
+    }
+}
+
