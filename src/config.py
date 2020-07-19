bf8a8dd71517067789dcd8835c7b7be313fd5035
Sreejith <sreejimunthikodu@yahoo.com>
2020-07-18 17:47:14 -0700

    read.py can connect and read from postgres and mysql on EC2

--- src/config.py
+++ src/config.py
@@ -3,19 +3,21 @@ import os
 DB_DETAILS = {
     'dev': {
         'mysql': {
-            'user': my_admin,
-            'password': mypassword,
+            'username': 'my_admin',
+            'password': 'mypassword',
             'host': '3.96.15.164',
             'port': 3306,
-            'database': covid_db_mysql
-        }
+            'database': 'covid_db_mysql'
+        },
         'psql': {
-            'user': my_admin,
-            'password': mypassword,
+            'username': 'my_admin',
+            'password': 'mypassword',
             'host': '3.96.15.164',
             'port': 5432,
-            'database': covid_db_psql
+            'database': 'covid_db_psql'
         }
     }
 }
 
+POPULATION_TABLE = 'world_population'
+COVID_TABLE = 'covid_data'
--- src/read.py
+++ src/read.py
@@ -1,10 +1,41 @@
+import config
+from sqlalchemy import create_engine
+import pandas as pd
+# import pymysql
+
+
 # Connect MYSQL database
+def connect_to_source_db(DB_DETAILS, source):
+    # Get the connection details from config file
+    if source == 'mysql':
+        dev = DB_DETAILS['dev']['mysql']
+        connection_uri = f'mysql+pymysql://{dev["username"]}:{dev["password"]}@{dev["host"]}:{dev["port"]}/{dev["database"]}'
+    elif source == 'psql':
+        dev = DB_DETAILS['dev']['psql']
+        connection_uri = f'postgresql://{dev["username"]}:{dev["password"]}@{dev["host"]}:{dev["port"]}/{dev["database"]}'
+
+    db_engine = create_engine(connection_uri)
+
+    return db_engine
 
 
+# Read from source databases
+def extract_table_to_pandas(tablename, db_engine):
+    query = f'SELECT * FROM {tablename}'
+    df = pd.read_sql(query, db_engine)
 
-# Connect PostgreSQL database
+    return df
 
 
-# Read from MYSQL database
+if __name__ == "__main__":
+    print('Reading from PSQL...')
+    db_engine_psql = connect_to_source_db(config.DB_DETAILS, source='psql')
+    df_pop = extract_table_to_pandas(config.POPULATION_TABLE, db_engine_psql)
+    print(df_pop)
 
-# Read from PostgreSQL database
+    print('\nReading from MYSQL...')
+    db_engine_mysql = connect_to_source_db(config.DB_DETAILS, source='mysql')
+    df_covid = extract_table_to_pandas(config.COVID_TABLE, db_engine_mysql)
+    print(df_covid)
+    
+
